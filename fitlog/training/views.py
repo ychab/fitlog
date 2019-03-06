2cf16277e5ccf3786c4678210c6cc452551c727e
Yannick Chabbert <yannick.chabbert@makina-corpus.com>
2019-03-06 21:35:50 +0100

    up routine list view

--- fitlog/training/views.py
+++ fitlog/training/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from rest_framework.filters import OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from fitlog.training.models import Exercise, Routine, TrainingLog, Workout
@@ -31,15 +32,24 @@ class WorkoutDetailView(LoginRequiredMixin, generic.DetailView):
 class RoutineViewSet(ModelViewSet):
     queryset = Routine.objects.all()
     serializer_class = RoutineSerializer
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ('name',)
+    ordering = ('name',)
 
 
 class ExerciseViewSet(ModelViewSet):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ('name',)
+    ordering = ('name',)
 
 
 class WorkoutViewSet(ModelViewSet):
     queryset = Workout.objects.all()
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ('name',)
+    ordering = ('name',)
 
     def get_serializer_class(self):
         if self.action in ['create', 'partial_update', 'update']:
--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,23 @@ function sb_admin() {
     .pipe(dest('fitlog/static/sb-admin/'));
 }
 
+function vue() {
+  return src('./node_modules/vue/**/*')
+    .pipe(dest('fitlog/static/vue/'));
+}
+
+function vue_cookies() {
+  return src('./node_modules/vue-cookies/**/*')
+    .pipe(dest('fitlog/static/vue-cookies/'));
+}
+
+function axios() {
+  return src('./node_modules/axios/**/*')
+    .pipe(dest('fitlog/static/axios/'));
+}
+
 exports.sb_admin = sb_admin;
-exports.default = series(sb_admin);
+exports.vue = vue;
+exports.vue_cookies = vue_cookies;
+exports.axios = axios;
+exports.default = series(sb_admin, vue, vue_cookies, axios);
